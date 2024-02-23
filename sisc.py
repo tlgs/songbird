03a2c9b953383b026e52bc64bf4f85807a37fe04
Tiago Seabra <tlgs@users.noreply.github.com>
2024-02-23 05:23:59 +0000

    Clean up app style
    Sort of inspired by tconbeer/harlequin style. I cba to change the
    default $primary, $secondary, etc. values though.
    
    Also did some refactoring and cleaned up a wee bit of code.

--- sisc.py
+++ sisc.py
@@ -9,16 +9,14 @@ from aiohttp import web
 from textual import on, work
 from textual.app import App
 from textual.binding import Binding
-from textual.containers import Center, Container, Horizontal, Vertical
+from textual.containers import Container, Horizontal, Vertical
 from textual.widgets import DataTable, Static
 
 gi.require_version("Tracker", "3.0")
 from gi.repository import Tracker  # noqa: E402
 
-SISC_PORT = 8080
 
-
-def this_ip():
+def self_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
 
@@ -70,67 +68,105 @@ class AlbumList(DataTable):
 
 class ControllerApp(App):
     CSS = """
+    $primary: #feffac;
+
+    Screen {
+      layout: horizontal;
+    }
+
     LoadingIndicator {
-        background: black 0%;
+      background: $background 0%;
     }
 
     AlbumList {
-        width: 60%;
-        overflow-x: hidden;
-        scrollbar-size-vertical: 1;
+      width: 7fr;
+      overflow-x: hidden;
+
+      scrollbar-size-vertical: 1;
+
+      scrollbar-background: $background-lighten-1;
+      scrollbar-background-hover: $background-lighten-1;
+      scrollbar-background-active: $background-lighten-1;
+
+      scrollbar-color: $panel;
+      scrollbar-color-hover: $primary;
+      scrollbar-color-active: $primary;
+
+      & > .datatable--header,
+      & > .datatable--header-hover {
+        text-style: bold;
+        background: $background 0%;
+        color: $primary;
+      }
+
+      & > .datatable--cursor {
+        background: $primary
+      }
+
+      & > .datatable--hover {
+        background: $boost;
+      }
     }
 
-    #now-playing {
-        width: 80%;
-        margin-top: 1;
+    Vertical {
+      width: 3fr;
+      align: center middle;
+
+      #now-playing {
+        margin: 2 0 0 2;
+        padding: 0 1;
         content-align: center middle;
+      }
     }
 
     #status-bar {
-        height: 1;
-        layout: grid;
-        grid-size: 3;
-        grid-columns: 2fr 1fr 1fr;
-    }
+      height: 1;
+      layout: grid;
+      grid-size: 3;
+      grid-columns: 1fr 1fr 1fr;
 
-    #sonos-status, #http-status {
+      & > * {
         text-align: center;
+      }
     }
     """
 
     BINDINGS = [
-        Binding("q", "quit", "Quit", show=False, priority=True),
+        Binding("q", "quit", "Quit", priority=True),
     ]
 
     def __init__(self):
-        self.sonos = None
-        self.http_runner = None
+        self.library = {}
         self.music_dir = platformdirs.user_music_dir()
-        self.controller_ip = this_ip()
+
+        self.sonos = None
+
+        self.http_runner = None
+        self.http_host = self_ip()
+        self.http_port = 8080
+
         super().__init__()
 
     def compose(self):
-        with Horizontal():
-            yield AlbumList(cursor_type="row")
+        yield AlbumList(cursor_type="row")
 
-            with Vertical():
-                yield Center(Static("No music selected", id="now-playing"))
+        with Vertical():
+            yield Static("No music selected", id="now-playing")
+            yield Container()
+            with Horizontal(id="status-bar"):
                 yield Container()
-                with Horizontal(id="status-bar"):
-                    yield Static()
-                    yield Static("Sonos [red]⬤[/red]", id="sonos-status")
-                    yield Static("HTTP [red]⬤[/red]", id="http-status")
+                yield Static("Sonos [red]⬤[/red]", id="sonos-status")
+                yield Static("HTTP [red]⬤[/red]", id="http-status")
 
     def on_mount(self):
         self.query_one(AlbumList).loading = True
 
         self.load_data()
         self.find_sonos()
-        self.spawn_http(self.controller_ip, SISC_PORT)
+        self.spawn_http(self.http_host, self.http_port)
 
     @work(thread=True)
     def load_data(self):
-        self.library = {}
         records = fetch_music()
         for artist, album, _, location in sorted(records):
             t = artist, album
@@ -152,7 +188,7 @@ class ControllerApp(App):
     @work(thread=True)
     def find_sonos(self):
         self.sonos, *_ = soco.discover()
-        self.query_one("#sonos-status").update("Sonos [green]⬤[/green]")
+        self.query_one("#sonos-status").update("Sonos [#45ffca]⬤[/#45ffca]")
 
         self.call_from_thread(self.set_interval, 1, self.update_current_track)
 
@@ -194,28 +230,32 @@ class ControllerApp(App):
         site = web.TCPSite(self.http_runner, host, port)
         await site.start()
 
-        self.query_one("#http-status").update("HTTP [green]⬤[/green]")
+        self.query_one("#http-status").update("HTTP [#45ffca]⬤[/#45ffca]")
 
     @on(DataTable.RowSelected)
     def select_album(self, event):
         t = event.control.get_row(event.row_key)
-        self.play_album(*t)
+
+        if self.sonos:
+            self.play_album(*t)
 
     @work(thread=True, group="control", exclusive=True)
     def play_album(self, album, artist):
         self.sonos.clear_queue()
 
         for location in self.library[artist, album]:
-            uri = f"http://{self.controller_ip}:{SISC_PORT}{location}"
+            uri = f"http://{self.http_host}:{self.http_port}{location}"
             self.sonos.add_uri_to_queue(uri)
 
         self.sonos.play_from_queue(0)
 
     async def on_unmount(self):
-        self.sonos.stop()
-        self.sonos.clear_queue()
+        if self.sonos:
+            self.sonos.stop()
+            self.sonos.clear_queue()
 
-        await self.http_runner.cleanup()
+        if self.http_runner:
+            await self.http_runner.cleanup()
 
 
 def main():
